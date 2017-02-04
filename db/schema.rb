f78b5e4eddf24f7b387b8563299b748b3d64703c
Aditya Swaroop <Aditya.Swaroop@craneww.com>
2017-02-04 10:48:53 +0530

    change in database file

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160229090808) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
@@ -39,7 +36,7 @@ ActiveRecord::Schema.define(version: 20160229090808) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
